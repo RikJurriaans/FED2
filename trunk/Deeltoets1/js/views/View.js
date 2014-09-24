9d060c439a6e59b9f47d12c66ab8298077e683e8
Rik Jurriaans <rik@jurriaans.org>
2014-09-24 11:42:36 +0200

    no more templates in the index.html.

--- trunk/Deeltoets1/js/views/View.js
+++ trunk/Deeltoets1/js/views/View.js
@@ -1,5 +1,7 @@
 var View = function(templateId, data) {
+    // Load view.
     var elem = document.getElementById(templateId);
+    console.log(elem);
     Transparency.render(elem, data);
 
     return {
